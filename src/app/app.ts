f4915dcfb8e3cd3132222967eabf7b54b16cfba6
mennaAllahSayedd <mennasayed4040@gmail.com>
2026-03-08 13:12:57 +0200

    add tailwind library & navbar componet and  employee_sidebar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {EmployeeSidebar} from '../app/shared/employee-sidebar/employee-sidebar'
+import { Navbar } from './shared/navbar/navbar';
+import { NotFound } from './shared/not-found/not-found';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,EmployeeSidebar ,Navbar,NotFound],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('HRMS_ANG');
+
+  letters: string[] = ['A', 'B', 'C', 'D'];
 }
---
+++ src/app/shared/employee-sidebar/employee-sidebar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-sidebar',
+  imports: [],
+  templateUrl: './employee-sidebar.html',
+  styleUrl: './employee-sidebar.css',
+})
+export class EmployeeSidebar {}
